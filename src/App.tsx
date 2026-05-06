0da05f1d8e06cbc370767c19ce58f81f1d009d65
bkbrahim960-ship-it <bkbrahim960@gmail.com>
2026-05-07 00:35:34 +0200

    chore: re-trigger deployment build to clear cache

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Toaster as Sonner } from "@/components/ui/sonner";
@@ -169,3 +170,4 @@ const App = () => {
 };
 
 export default App;
+// Re-trigger build 2026-05-07
