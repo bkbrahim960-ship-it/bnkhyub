79d631a6f2680258b17b4457a0fbfe6f8507cf5f
bkbrahim960-ship-it <bkbrahim960@gmail.com>
2026-05-27 03:55:09 +0200

    Update Kabyle movies and series playlist and URLs

--- src/services/customContent.ts
+++ src/services/customContent.ts
@@ -1,229 +1,226 @@
 export const KABYLE_CONTENT = [
-{
-  id: 'm-hmimi-lyes',
+  {
+    id: 'm-hmimi-lyes',
     title: 'Ḥmimi d Lyes',
-      description: 'Ḥmimi d Lyes, d asaru n unecreḥ, yettales-d yiwet n tmacahut yellan deg zman anda llan idinuẓuṛen !',
-        thumbnail: 'https://abdessah15.github.io/Asekles/hmimi.jpg',
-          category: 'Uskan • Tasawant',
-            year: '2025',
-              rating: 5.0,
-                isDubbed: true,
-                  videoUrl: 'https://odysee.com/$/embed/@asekles:d/Hmimi-d-lyes:e'
-},
-{
-  id: 'm-amechah-new',
+    description: 'Ḥmimi d Lyes, d asaru n unecreḥ, yettales-d yiwet n tmacahut yellan deg zman anda llan idinuẓuṛen !',
+    thumbnail: 'https://abdessah15.github.io/Asekles/hmimi.jpg',
+    category: 'Uskan • Tasawant',
+    year: '2025',
+    rating: 5.0,
+    isDubbed: true,
+    videoUrl: 'https://odysee.com/$/embed/@asekles:d/Hmimi-d-lyes:e'
+  },
+  {
+    id: 'm-amechah-new',
     title: 'Amecḥaḥ',
-      description: 'D asaru n uḍsa n teqbaylit yettmeslayen ɣef yiwen n umechaḥ i iḥemmlen idrimen aṭas.',
-        thumbnail: 'https://i.ytimg.com/vi/xJTRs4u-FgU/hq720.jpg',
-          category: 'Uḍsa • Akalas',
-            year: '2010',
-              rating: 4.8,
-                isDubbed: false,
-                  videoUrl: 'https://www.youtube.com/watch?v=xJTRs4u-FgU'
-},
-{
-  id: 'm-ikerri-izimer',
+    description: 'D asaru n uḍsa n teqbaylit yettmeslayen ɣef yiwen n umechaḥ i iḥemmlen idrimen aṭas.',
+    thumbnail: 'https://i.ytimg.com/vi/xJTRs4u-FgU/hq720.jpg',
+    category: 'Uḍsa • Akalas',
+    year: '2010',
+    rating: 4.8,
+    isDubbed: false,
+    videoUrl: 'https://www.youtube.com/watch?v=xJTRs4u-FgU'
+  },
+  {
+    id: 'm-ikerri-izimer',
     title: 'Ikerri izimer',
-      description: 'Asaru n uḍsa d unecreḥ i dderyet (Yettwasuḍ s teqbaylit).',
-        thumbnail: 'https://abdessah15.github.io/Asekles/ikerri.jpg',
-          category: 'Uḍsa • Uskan',
-            year: '2024',
-              rating: 4.8,
-                isDubbed: true,
-                  videoUrl: 'https://odysee.com/$/embed/@asekles:d/Ixerri-izimer:1'
-},
-{
-  id: 'm-at-dawuzru',
+    description: 'Asaru n uḍsa d unecreḥ i dderyet (Yettwasuḍ s teqbaylit).',
+    thumbnail: 'https://abdessah15.github.io/Asekles/ikerri.jpg',
+    category: 'Uḍsa • Uskan',
+    year: '2024',
+    rating: 4.8,
+    isDubbed: true,
+    videoUrl: 'https://odysee.com/$/embed/@asekles:d/Ixerri-izimer:1'
+  },
+  {
+    id: 'm-at-dawuzru',
     title: 'At Dawuzru',
-      description: 'D afilm n tmaziɣt i d-yettmeslayen ɣef tudert d imeṭṭawen n leqbayel.',
-        thumbnail: 'https://github.com/amazighkab/cinema-kabyle/blob/master/img/atdawuzru.jpg?raw=true',
-          category: 'Amazruy • Tasawant',
-            year: '2015',
-              rating: 4.6,
-                isDubbed: false,
-                  videoUrl: 'https://vk.com/video_ext.php?oid=267205663&id=169867258&hash=c9c250a4dfcef4f9&hd=2'
-},
-{
-  id: 'm-bebelilly',
+    description: 'D afilm n tmaziɣt i d-yettmeslayen ɣef tudert d imeṭṭawen n leqbayel.',
+    thumbnail: 'https://github.com/amazighkab/cinema-kabyle/blob/master/img/atdawuzru.jpg?raw=true',
+    category: 'Amazruy • Tasawant',
+    year: '2015',
+    rating: 4.6,
+    isDubbed: false,
+    videoUrl: 'https://vk.com/video_ext.php?oid=267205663&id=169867258&hash=c9c250a4dfcef4f9&hd=2'
+  },
+  {
+    id: 'm-bebelilly',
     title: 'Bebe Lili',
-      description: 'Afilm n uḍsa i d-yessefkayen tasekla n teqbaylit.',
-        thumbnail: 'https://th.bing.com/th/id/OIP.54aLHygZ_XsYmkiRRv2c7gAAAA?w=115&h=180&c=7&r=0&o=7&pid=1.7&rm=3',
-          category: 'Uḍsa • Akalas',
-            year: '2012',
-              rating: 4.4,
-                isDubbed: false,
-                  videoUrl: 'https://vk.com/video_ext.php?oid=267205663&id=169821305&hash=ae3eb8506578e6cb&hd=3'
-},
-{
-  id: 'm-iqjanimcac',
+    description: 'Afilm n uḍsa i d-yessefkayen tasekla n teqbaylit.',
+    thumbnail: 'https://th.bing.com/th/id/OIP.54aLHygZ_XsYmkiRRv2c7gAAAA?w=115&h=180&c=7&r=0&o=7&pid=1.7&rm=3',
+    category: 'Uḍsa • Akalas',
+    year: '2012',
+    rating: 4.4,
+    isDubbed: false,
+    videoUrl: 'https://vk.com/video_ext.php?oid=267205663&id=169821305&hash=ae3eb8506578e6cb&hd=3'
+  },
+  {
+    id: 'm-iqjanimcac',
     title: 'Iqjan Imcac',
-      description: 'Afilm n teqbaylit i d-yettawin ɣef tmetti d taddart.',
-        thumbnail: 'https://github.com/amazighkab/cinema-kabyle/blob/master/img/iqjanimcac.jpg?raw=true',
-          category: 'Imetti • Tasawant',
-            year: '2014',
-              rating: 4.3,
-                isDubbed: false,
-                  videoUrl: 'https://vk.com/video_ext.php?oid=267205663&id=169798608&hash=688c354f4d01f753&hd=1'
-},
-{
-  id: 'm-vriroch',
+    description: 'Afilm n teqbaylit i d-yettawin ɣef tmetti d taddart.',
+    thumbnail: 'https://github.com/amazighkab/cinema-kabyle/blob/master/img/iqjanimcac.jpg?raw=true',
+    category: 'Imetti • Tasawant',
+    year: '2014',
+    rating: 4.3,
+    isDubbed: false,
+    videoUrl: 'https://vk.com/video_ext.php?oid=267205663&id=169798608&hash=688c354f4d01f753&hd=1'
+  },
+  {
+    id: 'm-vriroch',
     title: 'Vriroch',
-      description: 'Tamacahut n tmeṭṭut d tettebwiḍ n wussan n leqbayel.',
-        thumbnail: 'https://github.com/amazighkab/cinema-kabyle/blob/master/img/vriroch.jpg?raw=true',
-          category: 'Amazruy • Tasawant',
-            year: '2011',
-              rating: 4.2,
-                isDubbed: false,
-                  videoUrl: 'https://vk.com/video_ext.php?oid=267205663&id=169797301&hash=7cf1f92039f951fc&hd=1'
-},
-{
-  id: 'm-alidwali',
+    description: 'Tamacahut n tmeṭṭut d tettebwiḍ n wussan n leqbayel.',
+    thumbnail: 'https://github.com/amazighkab/cinema-kabyle/blob/master/img/vriroch.jpg?raw=true',
+    category: 'Amazruy • Tasawant',
+    year: '2011',
+    rating: 4.2,
+    isDubbed: false,
+    videoUrl: 'https://vk.com/video_ext.php?oid=267205663&id=169797301&hash=7cf1f92039f951fc&hd=1'
+  },
+  {
+    id: 'm-alidwali',
     title: 'Ali d Wali',
-      description: 'Afilm n uḍsa d tamedyazt n teqbaylit.',
-        thumbnail: 'https://github.com/amazighkab/cinema-kabyle/blob/master/img/alidwali.jpg?raw=true',
-          category: 'Uḍsa • Akalas',
-            year: '2009',
-              rating: 4.5,
-                isDubbed: false,
-                  videoUrl: 'https://luluvid.com/e/ftbi9zs1g70i'
-},
-{
-  id: 'm-iferfucen',
+    description: 'Afilm n uḍsa d tamedyazt n teqbaylit.',
+    thumbnail: 'https://github.com/amazighkab/cinema-kabyle/blob/master/img/alidwali.jpg?raw=true',
+    category: 'Uḍsa • Akalas',
+    year: '2009',
+    rating: 4.5,
+    isDubbed: false,
+    videoUrl: 'https://luluvid.com/ftbi9zs1g70i'
+  },
+  {
+    id: 'm-iferfucen',
     title: 'Iferfucen',
-      description: 'Asaru n uḍsa i d-yettwanefk ɣef yimeṭṭawen n leqbayel.',
-        thumbnail: 'https://github.com/amazighkab/cinema-kabyle/blob/master/img/iferfucen.jpg?raw=true',
-          category: 'Uḍsa • Akalas',
-            year: '2013',
-              rating: 4.3,
-                isDubbed: false,
-                  videoUrl: 'https://vk.com/video_ext.php?oid=267205663&id=169791511&hash=449213e4d507224e&hd=1'
-},
-{
-  id: 'm-skarfez',
+    description: 'Asaru n uḍsa i d-yettwanefk ɣef yimeṭṭawen n leqbayel.',
+    thumbnail: 'https://github.com/amazighkab/cinema-kabyle/blob/master/img/iferfucen.jpg?raw=true',
+    category: 'Uḍsa • Akalas',
+    year: '2013',
+    rating: 4.3,
+    isDubbed: false,
+    videoUrl: 'https://vk.com/video_ext.php?oid=267205663&id=169791511&hash=449213e4d507224e&hd=1'
+  },
+  {
+    id: 'm-skarfez',
     title: 'Skarfez',
-      description: 'Afilm n tmaziɣt i d-yettmeslayen ɣef tazmert d tefsut n teqbaylit.',
-        thumbnail: 'https://github.com/amazighkab/cinema-kabyle/blob/master/img/skarfez.jpg?raw=true',
-          category: 'Amazruy • Tasawant',
-            year: '2016',
-              rating: 4.4,
-                isDubbed: false,
-                  videoUrl: 'https://vk.com/video_ext.php?oid=267205663&id=169786324&hash=804f4a5c7470ea93&hd=1'
-},
-{
-  id: 'm-mqidech',
+    description: 'Afilm n tmaziɣt i d-yettmeslayen ɣef tazmert d tefsut n teqbaylit.',
+    thumbnail: 'https://github.com/amazighkab/cinema-kabyle/blob/master/img/skarfez.jpg?raw=true',
+    category: 'Amazruy • Tasawant',
+    year: '2016',
+    rating: 4.4,
+    isDubbed: false,
+    videoUrl: 'https://vk.com/video_ext.php?oid=267205663&id=169786324&hash=804f4a5c7470ea93&hd=1'
+  },
+  {
+    id: 'm-mqidech',
     title: 'Mqidech',
-      description: 'Afilm n uḍsa i d-yesskanen tura n teqbaylit.',
-        thumbnail: 'https://github.com/amazighkab/cinema-kabyle/blob/master/img/mqidech.jpg?raw=true',
-          category: 'Uḍsa • Akalas',
-            year: '2008',
-              rating: 4.2,
-                isDubbed: false,
-                  videoUrl: 'https://vk.com/video_ext.php?oid=267205663&id=169785631&hash=7657c6a4d329bba1&hd=1'
-},
-
-/* UQload embeds */
-{
-  id: 'm-uq-wr2rr1s0488m',
+    description: 'Afilm n uḍsa i d-yesskanen tura n teqbaylit.',
+    thumbnail: 'https://github.com/amazighkab/cinema-kabyle/blob/master/img/mqidech.jpg?raw=true',
+    category: 'Uḍsa • Akalas',
+    year: '2008',
+    rating: 4.2,
+    isDubbed: false,
+    videoUrl: 'https://vk.com/video_ext.php?oid=267205663&id=169785631&hash=7657c6a4d329bba1&hd=1'
+  },
+  {
+    id: 'm-uq-wr2rr1s0488m',
     title: 'Afilm 1',
-      description: 'Afilm n teqbaylit (UQload).',
-        thumbnail: 'https://kabyletv.alwaysdata.net/image/20186089.jpg',
-          category: 'Afilm',
-            year: '—',
-              rating: 4.5,
-                isDubbed: false,
-                  videoUrl: 'https://uqload.bz/embed-wr2rr1s0488m.html'
-},
-{
-  id: 'm-uq-cz2hwbawnuwe',
+    description: 'Afilm n teqbaylit (UQload).',
+    thumbnail: 'https://kabyletv.alwaysdata.net/image/20186089.jpg',
+    category: 'Afilm',
+    year: '—',
+    rating: 4.5,
+    isDubbed: false,
+    videoUrl: 'https://uqload.com/embed-wr2rr1s0488m.html'
+  },
+  {
+    id: 'm-uq-cz2hwbawnuwe',
     title: 'Afilm 2',
-      description: 'Afilm n teqbaylit (UQload).',
-        thumbnail: 'https://kabyletv.alwaysdata.net/image/96d8323186702e3b8a51fed19adb7a6d.jpg',
-          category: 'Afilm',
-            year: '—',
-              rating: 4.5,
-                isDubbed: false,
-                  videoUrl: 'https://uqload.bz/embed-cz2hwbawnuwe.html'
-},
-{
-  id: 'm-uq-0jc0s8bdovy8',
+    description: 'Afilm n teqbaylit (UQload).',
+    thumbnail: 'https://kabyletv.alwaysdata.net/image/96d8323186702e3b8a51fed19adb7a6d.jpg',
+    category: 'Afilm',
+    year: '—',
+    rating: 4.5,
+    isDubbed: false,
+    videoUrl: 'https://uqload.com/embed-cz2hwbawnuwe.html'
+  },
+  {
+    id: 'm-uq-0jc0s8bdovy8',
     title: 'Afilm 3',
-      description: 'Afilm n teqbaylit (UQload).',
-        thumbnail: 'https://kabyletv.alwaysdata.net/image/download.jpg',
-          category: 'Afilm',
-            year: '—',
-              rating: 4.5,
-                isDubbed: false,
-                  videoUrl: 'https://uqload.bz/embed-0jc0s8bdovy8.html'
-},
-{
-  id: 'm-uq-7fiyh3verof3',
+    description: 'Afilm n teqbaylit (UQload).',
+    thumbnail: 'https://kabyletv.alwaysdata.net/image/download.jpg',
+    category: 'Afilm',
+    year: '—',
+    rating: 4.5,
+    isDubbed: false,
+    videoUrl: 'https://uqload.com/embed-0jc0s8bdovy8.html'
+  },
+  {
+    id: 'm-uq-7fiyh3verof3',
     title: 'Afilm 4',
-      description: 'Afilm n teqbaylit (UQload).',
-        thumbnail: 'https://kabyletv.alwaysdata.net/image/p1.jpg',
-          category: 'Afilm',
-            year: '—',
-              rating: 4.5,
-                isDubbed: false,
-                  videoUrl: 'https://uqload.bz/embed-7fiyh3verof3.html'
-},
-{
-  id: 'm-uq-eg8hie3ioj54',
+    description: 'Afilm n teqbaylit (UQload).',
+    thumbnail: 'https://kabyletv.alwaysdata.net/image/p1.jpg',
+    category: 'Afilm',
+    year: '—',
+    rating: 4.5,
+    isDubbed: false,
+    videoUrl: 'https://uqload.com/embed-7fiyh3verof3.html'
+  },
+  {
+    id: 'm-uq-eg8hie3ioj54',
     title: 'Afilm 5',
-      description: 'Afilm n teqbaylit (UQload).',
-        thumbnail: 'https://kabyletv.alwaysdata.net/image/19099924.jpg',
-          category: 'Afilm',
-            year: '—',
-              rating: 4.5,
-                isDubbed: false,
-                  videoUrl: 'https://uqload.bz/embed-eg8hie3ioj54.html'
-},
-{
-  id: 'm-uq-k0e4kayth43b',
+    description: 'Afilm n teqbaylit (UQload).',
+    thumbnail: 'https://kabyletv.alwaysdata.net/image/19099924.jpg',
+    category: 'Afilm',
+    year: '—',
+    rating: 4.5,
+    isDubbed: false,
+    videoUrl: 'https://uqload.com/embed-eg8hie3ioj54.html'
+  },
+  {
+    id: 'm-uq-k0e4kayth43b',
     title: 'Afilm 6',
-      description: 'Afilm n teqbaylit (UQload).',
-        thumbnail: 'https://kabyletv.alwaysdata.net/image/51eJoWwodML._AC_UF1000,1000_QL80_.jpg',
-          category: 'Afilm',
-            year: '—',
-              rating: 4.5,
-                isDubbed: false,
-                  videoUrl: 'https://uqload.bz/embed-k0e4kayth43b.html'
-},
-{
-  id: 'm-uq-45cizb3g988j',
+    description: 'Afilm n teqbaylit (UQload).',
+    thumbnail: 'https://kabyletv.alwaysdata.net/image/51eJoWwodML._AC_UF1000,1000_QL80_.jpg',
+    category: 'Afilm',
+    year: '—',
+    rating: 4.5,
+    isDubbed: false,
+    videoUrl: 'https://uqload.com/embed-k0e4kayth43b.html'
+  },
+  {
+    id: 'm-uq-45cizb3g988j',
     title: 'Afilm 7',
-      description: 'Afilm n teqbaylit (UQload).',
-        thumbnail: 'https://kabyletv.alwaysdata.net/image/qezboul.jpg',
-          category: 'Afilm',
-            year: '—',
-              rating: 4.5,
-                isDubbed: false,
-                  videoUrl: 'https://uqload.bz/embed-45cizb3g988j.html'
-},
-{
-  id: 'm-uq-907jor35z6se',
+    description: 'Afilm n teqbaylit (UQload).',
+    thumbnail: 'https://kabyletv.alwaysdata.net/image/qezboul.jpg',
+    category: 'Afilm',
+    year: '—',
+    rating: 4.5,
+    isDubbed: false,
+    videoUrl: 'https://uqload.com/embed-45cizb3g988j.html'
+  },
+  {
+    id: 'm-uq-907jor35z6se',
     title: 'Afilm 8',
-      description: 'Afilm n teqbaylit (UQload).',
-        thumbnail: 'https://kabyletv.alwaysdata.net/image/69199338_af.jpg',
-          category: 'Afilm',
-            year: '—',
-              rating: 4.5,
-                isDubbed: false,
-                  videoUrl: 'https://uqload.bz/embed-907jor35z6se.html'
-},
-{
-  id: 'm-uq-aoxz0qpfb7q3',
+    description: 'Afilm n teqbaylit (UQload).',
+    thumbnail: 'https://kabyletv.alwaysdata.net/image/69199338_af.jpg',
+    category: 'Afilm',
+    year: '—',
+    rating: 4.5,
+    isDubbed: false,
+    videoUrl: 'https://uqload.com/embed-907jor35z6se.html'
+  },
+  {
+    id: 'm-uq-aoxz0qpfb7q3',
     title: 'Afilm 9',
-      description: 'Afilm n teqbaylit (UQload).',
-        thumbnail: 'https://kabyletv.alwaysdata.net/image/images.jpg',
-          category: 'Afilm',
-            year: '—',
-              rating: 4.5,
-                isDubbed: false,
-                  videoUrl: 'https://uqload.bz/embed-aoxz0qpfb7q3.html'
-}
+    description: 'Afilm n teqbaylit (UQload).',
+    thumbnail: 'https://kabyletv.alwaysdata.net/image/images.jpg',
+    category: 'Afilm',
+    year: '—',
+    rating: 4.5,
+    isDubbed: false,
+    videoUrl: 'https://uqload.com/embed-aoxz0qpfb7q3.html'
+  }
 ];
 
-// قائمة المسلسلات الكاملة مع حلقاتها
 export const MOCK_SERIES: MediaItem[] = [
   {
     id: 's-imuhuchen',
